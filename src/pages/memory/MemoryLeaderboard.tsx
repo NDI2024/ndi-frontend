b609e81baee7fbdca4f4d5fc123e366975ce8cef
Martin Fromont <62103174+mart1fromont@users.noreply.github.com>
2023-12-08 00:24:59 +0100

    Added leaderboard

--- src/pages/memory/MemoryLeaderboard.tsx
+++ src/pages/memory/MemoryLeaderboard.tsx
@@ -1,6 +1,7 @@
 import {UserDashboardLayout} from "layouts/dashboard/userDashboardLayout";
 import Table from "components/table/table";
 import {useTranslation} from "react-i18next";
+import {FaTrophy} from "react-icons/fa";
 
 const data = [
     {
@@ -71,12 +72,58 @@ export const MemoryLeaderboard = () => {
         },
     ]
 
+    const podium = data.sort((a, b) => a.rank - b.rank).slice(0, 3)
+
     return (
         <UserDashboardLayout>
-            <div className="p-4 pt-8 bg-blue-100 w-full h-full">
-                <h2 className="text-2xl font-bold text-left mb-3">{t('Label.Classment')}</h2>
+            <div className="p-4 pt-2 bg-primary-100 w-full h-full">
+                <h2 className="text-2xl font-bold text-left mb-3 sticky top-0 pt-6 bg-primary-100">
+                    {t('Label.Classment')}
+                    <hr className="my-2" />
+                </h2>
 
-                <Table data={myData.concat(data.sort((a, b) => a.rank - b.rank))} columns={columns}/>
+                <div className="flex items-center justify-center mb-8">
+                    <div className="flex flex-col items-center mr-8">
+                        <div className="flex justify-center items-center bg-white rounded-full p-2">
+                            <FaTrophy className="text-gray-500 text-2xl" />
+                        </div>
+                        <p className="text-center text-gray-500 font-bold text-lg">
+                            {podium[1].username}
+                            <br />
+                            <span className="text-gray-500 text-md">
+                                {podium[1].score}
+                            </span>
+                        </p>
+                    </div>
+                    <div className="flex flex-col items-center mr-8">
+                        <div className="flex justify-center items-center bg-white rounded-full p-2">
+                            <FaTrophy className="text-yellow-500 text-3xl" />
+                        </div>
+                        <p className="text-center text-yellow-500 font-bold text-2xl">
+                            {podium[0].username}
+                            <br />
+                            <span className="text-gray-500 text-xl">
+                                {podium[0].score}
+                            </span>
+                        </p>
+                    </div>
+                    <div className="flex flex-col items-center">
+                        <div className="flex justify-center items-center bg-white rounded-full p-2">
+                            <FaTrophy className="text-yellow-900 text-2xl" />
+                        </div>
+                        <p className="text-center text-yellow-900 font-bold">
+                            {podium[2].username}
+                            <br />
+                            <span className="text-xs text-gray-500">
+                                {podium[2].score}
+                            </span>
+                        </p>
+                    </div>
+                </div>
+
+                <div className="fade-in-up">
+                    <Table data={myData.concat(data.sort((a, b) => a.rank - b.rank))} columns={columns}/>
+                </div>
             </div>
         </UserDashboardLayout>
     )

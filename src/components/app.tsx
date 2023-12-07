531801c77fb689639d41847cf4699a5fe24c1cfc
Martin Fromont <62103174+mart1fromont@users.noreply.github.com>
2023-12-07 23:08:48 +0100

    Added card description

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -9,7 +9,6 @@ import {addBearerToken} from "utils/axios";
 import {GetMe} from "services/user/user";
 import {useDispatch} from "react-redux";
 import {setUser} from "store/actions/userActions";
-import {CardDescription} from "components/card/card-description";
 
 export const App = () => {
     const dispatch = useDispatch()
@@ -49,7 +48,6 @@ export const App = () => {
 
     return (
         <Flowbite theme={{theme: FlowbiteCustomTheme}}>
-            <CardDescription title="Je suis un titre d'exemple" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." image="/public/logo192.png" />
             <RouterProvider router={router}/>
         </Flowbite>
     )

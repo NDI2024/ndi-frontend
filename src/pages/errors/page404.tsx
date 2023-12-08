c29744929d1e57bc94ae2f566b380fbd3eacc835
Martin Fromont <62103174+mart1fromont@users.noreply.github.com>
2023-12-08 03:10:42 +0100

    Fixed super secret page not found tetris game page

--- src/pages/errors/page404.tsx
+++ src/pages/errors/page404.tsx
@@ -1,5 +1,7 @@
 import Tetris from "data/tetris/components/Tetris";
 import {UserDashboardLayout} from "layouts/dashboard/userDashboardLayout";
+import {Button} from "components/generic/button";
+import {FaExclamationTriangle} from "react-icons/fa";
 
 export const Page404 = () => {
     return (
@@ -16,6 +18,29 @@ export const Page404 = () => {
                           level,
                       }) => (
                         <div className="tetris-container">
+                            <div className="text-pixel text-2xl mr-4 mt-4">
+                                <p className="text-red-600">
+                                    <FaExclamationTriangle className="mr-2"/>
+                                    <span>
+                                        Erreur 404
+                                    </span>
+                                </p>
+
+                                <p className="text-xl">
+                                    Cette page n'existe pas
+                                </p>
+
+                                <Button
+                                    type="button"
+                                    className="mt-4"
+                                    onClick={() => {
+                                        window.history.back();
+                                    }}
+                                >
+                                    Retourner en lieu sûr
+                                </Button>
+                            </div>
+
                             <div className="mr-2">
                                 {level <= 1 && (
                                     <img src="/thermometer/thermometer1.png" alt="Niveau"/>

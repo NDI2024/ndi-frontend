5fd2f10ccb2bb05dc4bd3745df6bc13973605582
LouisVdwl <louis.vandewalle98@gmail.com>
2023-12-08 04:16:40 +0100

    add icon to replay

--- src/pages/memory/finishMemory.tsx
+++ src/pages/memory/finishMemory.tsx
@@ -2,6 +2,7 @@ import {CardData} from "types/global";
 import {CardDetails} from "components/card/cardDetails";
 import {Button} from "components/generic/button";
 import {useTranslation} from "react-i18next";
+import {GrPowerReset} from "react-icons/gr";
 
 interface Props {
     cards: CardData[]
@@ -16,15 +17,17 @@ export const FinishMemory = ({cards, reloadDatas}: Props) => {
             t.id === card.id
         )))
     return (
-        <div className={'flex flex-wrap gap-2 justify-center'}>
-            {
-                cardsFiltered.map((card: CardData) => {
-                    return <CardDetails card={card} key={card.id}/>
-                })
-            }
-
-            <Button type={'button'} className={'mt-4'} onClick={reloadDatas}>
+        <div className={'flex flex-col items-center'}>
+            <div className={'flex flex-wrap gap-2 justify-center'}>
                 {
+                    cardsFiltered.map((card: CardData) => {
+                        return <CardDetails card={card} key={card.id}/>
+                    })
+                }
+            </div>
+            <Button type={'button'} className={'mt-4 !w-fit text-white'} onClick={reloadDatas}>
+                <GrPowerReset className={'mr-2 text-white'}/>
+                    {
                     t('Label.Replay')
                 }
             </Button>
